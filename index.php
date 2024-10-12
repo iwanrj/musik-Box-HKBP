da7386f45364631220e6825388bb77f8809a80f3
Iwan R <44029043+iwanrj@users.noreply.github.com>
2024-10-12 16:27:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 $MYAPPNAME = '<NAMA-APLIKASI-KAMU>';
-$APIKEY = 'APIKEY-KAMU';
+$API = 'APIKEY-KAMU';
 $TYPE = 'json';
 $HOST = 'https://jemaathkbp.my.id/apk/api';
 $QUEST = 'no=<NOMOR-ENDE>&get=<KJ-atau-BE>';
+$CONN = $HOST.'/'?checked&apikey='.$API.'&'.$QUEST.'&type='.$TYPE;
 // BERSAMBUNG (TAHAP PENGEMBANGAN)
 // ......
 ?>
