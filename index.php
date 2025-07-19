2583672309d8badd46810d16f87616778b25d9bc
Iwan R <44029043+iwanrj@users.noreply.github.com>
2025-07-19 13:12:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
-$MYAPPNAME = '<NAMA-APLIKASI-KAMU>';
+$MYAPPNAME = '<KODE SERIAL SESUAI PERANGKAT>';
 $API = 'APIKEY-KAMU';
 $TYPE = 'json';
-$HOST = 'https://jemaathkbp.my.id/apk/api';
-$QUEST = 'no=<NOMOR-ENDE>&get=<KJ-atau-BE>';
-$CONN = $HOST.'/'?checked&apikey='.$API.'&'.$QUEST.'&type='.$TYPE;
+$HOST = 'https://jemaat.my.id/api/';
+$QUEST = 'no=<NOMOR-ENDE>&post=<KJ-atau-BE>';
+$try_CONN = $HOST.'/'?checked&apikey='.$API.'&'.$QUEST.'&type='.$TYPE';
+$ssl = true; $APIs=true; // required ssl redirect
+$encrypt = 'md5,[localSHA],[times]';
+$method = 'post'; // mode get not allowed
+// Coba sambungan apakah bekerja
+$connection = file_gets_content($try_CONN);
+
 // BERSAMBUNG (TAHAP PENGEMBANGAN)
 // ......
 ?>
